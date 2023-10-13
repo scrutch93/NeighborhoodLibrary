2419a81a9d168ee2e019c14fe8c09591e965ac6a
Steven <stevccru@icloud.com>
2023-10-13 15:58:13 -0400

    first commit

---
+++ src/main/java/com/pluralsight/Book.java
@@ -0,0 +1,65 @@
+package com.pluralsight;
+
+public class Book {
+
+    private int id;
+    private String isbn;
+
+    private String title;
+
+    private boolean isCheckedOut;
+
+    private String checkedOutTo;
+
+
+    public Book(int id, String isbn, String title, boolean isCheckedOut, String checkedOutTo){
+
+         this.id = id;
+         this.isbn = isbn;
+         this.title = title;
+         this.isCheckedOut = isCheckedOut;
+         this.checkedOutTo = checkedOutTo;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getIsbn() {
+        return this.isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getCheckedOutTo() {
+        return checkedOutTo;
+    }
+
+    public void setCheckedOutTo(String checkedOutTo) {
+        this.checkedOutTo = checkedOutTo;
+    }
+
+    public void checkOut(String name){
+     this.checkedOutTo = name;
+    this.isCheckedOut = true;
+    }
+
+    public void checkedIn(){
+        this.checkedOutTo="";
+
+    }
+}
---
+++ src/main/java/com/pluralsight/LibraryApplication.java
@@ -0,0 +1,19 @@
+package com.pluralsight;
+
+public class LibraryApplication {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void showAvailableBooks(){
+
+    }
+
+    public static void showCheckedOutBooks(){
+
+    }
+
+
+
+}
